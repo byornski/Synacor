f28d713a92e320111ab2c09dbbb17aab16a4b3f2
byornski <byornski@gmail.com>
2016-01-22 22:20:12 +0000

    Slightly refactored main program loop

--- program.py
+++ program.py
@@ -24,80 +24,82 @@ class prog:
         '''Run the program in memory'''
         #Iterate over operations in program order
         for op in self:
-            if op==0: # END
-                return
-            elif op==1: #set
-                reg,val = self.getNext('RV')
-                self.set(reg,val)
-            elif op==2: #push
-                a = self.getNext('V')
-                self.push(a)
-            elif op==3: #pop
-                a = self.getNext('R')
-                self.set(a,self.pop())
-            elif op==4: #eq - a<= b==c
-                a,b,c = self.getNext('RVV')
-                res = 1 if b==c else 0
-                self.set(a,res)
-            elif op==5: #gt
-                a,b,c = self.getNext('RVV')
-                res = 1 if b>c else 0
-                self.set(a,res)
-            elif op==6: #jmp
-                target = self.getNext('V')
-                self.jmp(target)
-            elif op==7: #JNZ
-                test,target = self.getNext('VV')
-                if (test != 0):
-                    self.jmp(target)
-            elif op==8: #JEZ 
-                test,target = self.getNext('VV')
-                if (test == 0):
-                    self.jmp(target)
-            elif op==9: #a <= b+c
-                a,b,c = self.getNext('RVV')
-                self.set(a,(b+c)%self.__mod)
-            elif op==10: #mult
-                a,b,c = self.getNext('RVV')
-                self.set(a,(b*c)%self.__mod)
-            elif op==11: #mod
-                a,b,c = self.getNext('RVV')
-                self.set(a,b%c)
-            elif op==12: #Binary and
-                a,b,c = self.getNext('RVV')
-                self.set(a,b&c)
-            elif op==13: #binary or
-                a,b,c = self.getNext('RVV')
-                self.set(a,b|c)
-            elif op==14:
-                a,b = self.getNext('RV')
-                self.set(a,~b%self.__mod)
-            elif op==15: #rmem
-                a,b = self.getNext('RV')
-                self.set(a,self.rmem(b))
-            elif op==16: #wmem
-                a,b = self.getNext('VV')
-                self.wmem(a,b)
-            elif op==17:
-                target = self.getNext('V')
-                self.push(self.Pos())
-                self.jmp(target)
-            elif op==18:
-                target = self.pop()
-                self.jmp(target)
-            elif op==19: # PRINT CHAR
-                s = self.getNext('V')
-                print(chr(s),end="")
-            elif op==20:
-                target = self.getNext('R')
-                st = sys.stdin.read(1)
-                self.set(target,ord(st))
-            elif op==21: #NOOP
-                pass 
-            else:
-                pass
-                raise Exception("Unknown Op", op, self.position)
+            self.performOp(op)
 
+    def performOp(self,op):
+        if op==0: # END
+            return
+        elif op==1: #set
+            reg,val = self.getNext('RV')
+            self.set(reg,val)
+        elif op==2: #push
+            a = self.getNext('V')
+            self.push(a)
+        elif op==3: #pop
+            a = self.getNext('R')
+            self.set(a,self.pop())
+        elif op==4: #eq - a<= b==c
+            a,b,c = self.getNext('RVV')
+            res = 1 if b==c else 0
+            self.set(a,res)
+        elif op==5: #gt
+            a,b,c = self.getNext('RVV')
+            res = 1 if b>c else 0
+            self.set(a,res)
+        elif op==6: #jmp
+            target = self.getNext('V')
+            self.jmp(target)
+        elif op==7: #JNZ
+            test,target = self.getNext('VV')
+            if (test != 0):
+                self.jmp(target)
+        elif op==8: #JEZ 
+            test,target = self.getNext('VV')
+            if (test == 0):
+                self.jmp(target)
+        elif op==9: #a <= b+c
+            a,b,c = self.getNext('RVV')
+            self.set(a,(b+c)%self.__mod)
+        elif op==10: #mult
+            a,b,c = self.getNext('RVV')
+            self.set(a,(b*c)%self.__mod)
+        elif op==11: #mod
+            a,b,c = self.getNext('RVV')
+            self.set(a,b%c)
+        elif op==12: #Binary and
+            a,b,c = self.getNext('RVV')
+            self.set(a,b&c)
+        elif op==13: #binary or
+            a,b,c = self.getNext('RVV')
+            self.set(a,b|c)
+        elif op==14:
+            a,b = self.getNext('RV')
+            self.set(a,~b%self.__mod)
+        elif op==15: #rmem
+            a,b = self.getNext('RV')
+            self.set(a,self.rmem(b))
+        elif op==16: #wmem
+            a,b = self.getNext('VV')
+            self.wmem(a,b)
+        elif op==17:
+            target = self.getNext('V')
+            self.push(self.Pos())
+            self.jmp(target)
+        elif op==18:
+            target = self.pop()
+            self.jmp(target)
+        elif op==19: # PRINT CHAR
+            s = self.getNext('V')
+            print(chr(s),end="")
+        elif op==20:
+            target = self.getNext('R')
+            st = sys.stdin.read(1)
+            self.set(target,ord(st))
+        elif op==21: #NOOP
+            pass 
+        else:
+            raise Exception("Unknown Op", op, self.position)
+        
         
     #Register interaction
     def set(self,reg,val):
