de10cf162d92bf8b57c4aee9137129bcedec04fd
byornski <byornski@gmail.com>
2016-07-21 21:36:22 +0100

    Refactored memory out of program

--- program.py
+++ program.py
@@ -5,30 +5,39 @@ import sys
 class prog:
     
     def __init__(self,filename):
-        '''Read filename as a program into memory'''
-        with open(filename,"rb") as f:
-            d = f.read()
-        self.data = map(''.join, zip(*[iter(d)]*2))
-        self.max_pos = len(self.data) - 1
-
-        self.position = 0
-        self.registers = [0]*8
-        self.stack = []
-
-        self.ops = operations(self)
-        self.endFlag = False
+        self.memory = memory(filename)
+        self.ops = operations(self.memory)
         
         
     def run(self):
         '''Run the program in memory'''
         #Iterate over operations in program order
-        for op in self:
+        for op in self.memory:
             self.ops.performOp(op)
-    
-    def setReg(self):
-        reg,val = self.getNext('RV')
-        self.set(reg,val)
+
+
+class memory:
+    #Initialise memory
+    def __init__(self,filename):
+        '''Initialise the memory from file'''
+        self.readFile(filename)
+
+        '''Zero stack and registers'''
+        self.registers = [0]*8
+        self.stack = []
+
+        self.endFlag = False
+
         
+    #File read
+    def readFile(self,filename):
+        '''Read filename as a program into memory'''
+        with open(filename,"rb") as f:
+            d = f.read()
+        self.data = map(''.join, zip(*[iter(d)]*2))
+        self.max_pos = len(self.data) - 1
+        self.position = 0
+
     #Register interaction
     def set(self,reg,val):
         """Set the value of a register"""
@@ -37,7 +46,7 @@ class prog:
     def get(self,reg):
         """Get the value of a register"""
         return self.registers[reg]
-
+    
     #Stack interactions
     def push(self,x):
         """Put something on to the stack"""
@@ -55,18 +64,6 @@ class prog:
     def wmem(self,loc,val):
         '''Write memory at location'''
         self.data[loc] = self._pack(val)
-
-    #Iterator for program data
-    def next(self):
-        '''Get the next line from the program at the current position unless end flag set'''
-        if self.endFlag: raise StopIteration
-        stri = self.data[self.position]
-        byte = self._unpack(stri)
-        self.position += 1
-        return byte
-
-    def __iter__(self):
-        return self
 
     #Operand reading instructions
     def getNext(self,vals_desired):
@@ -111,16 +108,26 @@ class prog:
         '''Return the current instruction pointer'''
         return self.position
 
+    #Iterator for program data
+    def next(self):
+        '''Get the next line from the program at the current position unless end flag set'''
+        if self.endFlag: raise StopIteration
+        stri = self.data[self.position]
+        byte = self._unpack(stri)
+        self.position += 1
+        return byte
 
-
-
+    def __iter__(self):
+        return self
+    
+        
 class operations:
     
     #All calculations are to be modulo this number
     __mod = 32768
 
-    def __init__(self,target):
-        self.parent = target
+    def __init__(self,mem):
+        self.mem = mem
         self.operation_dictionary = {
             0: self._end,
             1: self._set,
@@ -151,97 +158,97 @@ class operations:
         if op in self.operation_dictionary:
             self.operation_dictionary[op]()
         else:
-            raise Exception("Unknown Op", op, self.parent.position)
+            raise Exception("Unknown Op", op, self.mem.position)
     
     #operations
     def _end(self):
-        self.parent.endFlag = True
+        self.mem.endFlag = True
     
     def _set(self): #1
-        reg,val = self.parent.getNext('RV')
-        self.parent.set(reg,val)
+        reg,val = self.mem.getNext('RV')
+        self.mem.set(reg,val)
 
     def _push(self): #2
-        a = self.parent.getNext('V')
-        self.parent.push(a)
+        a = self.mem.getNext('V')
+        self.mem.push(a)
 
     def _pop(self): #3
-        a = self.parent.getNext('R')
-        self.parent.set(a,self.parent.pop())
+        a = self.mem.getNext('R')
+        self.mem.set(a,self.mem.pop())
         
     def _eq(self): #4
-        a,b,c = self.parent.getNext('RVV')
+        a,b,c = self.mem.getNext('RVV')
         res = 1 if b==c else 0
-        self.parent.set(a,res)
+        self.mem.set(a,res)
 
     def _gt(self): #5
-        a,b,c = self.parent.getNext('RVV')
+        a,b,c = self.mem.getNext('RVV')
         res = 1 if b>c else 0
-        self.parent.set(a,res)
+        self.mem.set(a,res)
         
     def _jmp(self): #6
-        target = self.parent.getNext('V')
-        self.parent.jmp(target)
+        target = self.mem.getNext('V')
+        self.mem.jmp(target)
         
     def _jnz(self): #7
-        test,target = self.parent.getNext('VV')
+        test,target = self.mem.getNext('VV')
         if (test != 0):
-            self.parent.jmp(target)
+            self.mem.jmp(target)
 
     def _jez(self): #8
-        test,target = self.parent.getNext('VV')
+        test,target = self.mem.getNext('VV')
         if (test == 0):
-            self.parent.jmp(target)
+            self.mem.jmp(target)
             
     def _add(self): #9
-        a,b,c = self.parent.getNext('RVV')
-        self.parent.set(a,(b+c)%self.__mod)
+        a,b,c = self.mem.getNext('RVV')
+        self.mem.set(a,(b+c)%self.__mod)
 
     def _mult(self): #10
-        a,b,c = self.parent.getNext('RVV')
-        self.parent.set(a,(b*c)%self.__mod)
+        a,b,c = self.mem.getNext('RVV')
+        self.mem.set(a,(b*c)%self.__mod)
 
     def _mod(self): #11
-        a,b,c = self.parent.getNext('RVV')
-        self.parent.set(a,b%c)
+        a,b,c = self.mem.getNext('RVV')
+        self.mem.set(a,b%c)
 
     def _and(self): #12
-        a,b,c = self.parent.getNext('RVV')
-        self.parent.set(a,b&c)
+        a,b,c = self.mem.getNext('RVV')
+        self.mem.set(a,b&c)
 
     def _or(self): #13
-        a,b,c = self.parent.getNext('RVV')
-        self.parent.set(a,b|c)
+        a,b,c = self.mem.getNext('RVV')
+        self.mem.set(a,b|c)
 
     def _not(self): #14
-        a,b = self.parent.getNext('RV')
-        self.parent.set(a,~b%self.__mod)
+        a,b = self.mem.getNext('RV')
+        self.mem.set(a,~b%self.__mod)
 
     def _rmem(self): #15
-        a,b = self.parent.getNext('RV')
-        self.parent.set(a,self.parent.rmem(b))
+        a,b = self.mem.getNext('RV')
+        self.mem.set(a,self.mem.rmem(b))
 
     def _wmem(self): #16
-        a,b = self.parent.getNext('VV')
-        self.parent.wmem(a,b)
+        a,b = self.mem.getNext('VV')
+        self.mem.wmem(a,b)
         
     def _call(self): #17
-        target = self.parent.getNext('V')
-        self.parent.push(self.parent.Pos())
-        self.parent.jmp(target)
+        target = self.mem.getNext('V')
+        self.mem.push(self.mem.Pos())
+        self.mem.jmp(target)
 
     def _ret(self): #18
-        target = self.parent.pop()
-        self.parent.jmp(target)
+        target = self.mem.pop()
+        self.mem.jmp(target)
 
     def _out(self): #19
-        s = self.parent.getNext('V')
+        s = self.mem.getNext('V')
         print(chr(s),end="")
         
     def _in(self): #20
-        target = self.parent.getNext('R')
+        target = self.mem.getNext('R')
         st = sys.stdin.read(1)
-        self.parent.set(target,ord(st))
+        self.mem.set(target,ord(st))
 
     def _noop(self): #21
         pass
